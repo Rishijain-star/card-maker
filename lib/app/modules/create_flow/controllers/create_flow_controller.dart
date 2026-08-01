57d5e0e17392c58954455e418550f872fa7814e1
Rishi Jain <rishijain@Rishis-Mac-mini.local>
2026-08-01 13:55:42 +0530

    Pass maxWidth: 1000 and maxHeight: 1000 to ImagePicker.pickImage to prevent Android LowMemoryKiller Activity termination

--- lib/app/modules/create_flow/controllers/create_flow_controller.dart
+++ lib/app/modules/create_flow/controllers/create_flow_controller.dart
@@ -743,7 +743,9 @@ class CreateFlowController extends GetxController {
       final file = await picker.pickImage(
         source: ImageSource.camera,
         preferredCameraDevice: CameraDevice.rear,
-        imageQuality: 90,
+        maxWidth: 1000,
+        maxHeight: 1000,
+        imageQuality: 85,
       );
       if (file != null) {
         final croppedPath = await Get.to<String>(
@@ -770,7 +772,9 @@ class CreateFlowController extends GetxController {
       final picker = ImagePicker();
       final file = await picker.pickImage(
         source: ImageSource.gallery,
-        imageQuality: 90,
+        maxWidth: 1000,
+        maxHeight: 1000,
+        imageQuality: 85,
       );
       if (file != null) {
         final croppedPath = await Get.to<String>(
